e0ea737564292a71270146adc6896ea5fc4b737b
gosling <prayush.twayana@gmail.com>
2025-08-27 18:43:46 +0545

    general

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,21 +3,8 @@ require("config")
 -- print("welcome back goose 🪿")
 -- print("------------------------------------------")
 
--- make the colorscheme transparent
-require("rose-pine").setup({
-  variant = "auto",                 -- Choose between 'main', 'moon', or 'dawn'
-  dark_variant = "main",
-  disable_background = true,        -- Makes the background transparent
-  disable_float_background = false, -- Makes floating windows transparent
-  bold_vert_split = false,
-  dim_nc_background = true,         -- Do not dim non-current window backgrounds
-  disable_italics = false,
-})
-
 vim.opt.backspace = { "indent", "eol", "start" }
 
-vim.cmd.colorscheme "rose-pine"
 vim.g.netrw_bufsettings = 'noma nomod nobl nowrap ro'
--- vim.o.cursorline = true
 vim.o.termguicolors = true
 vim.g.netrw_banner = 0
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,120 +1,134 @@
 -- Bootstrap lazy.nvim (the find filespackage manager)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-     vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Lazy.nvim
 require("lazy").setup({
-    -- colorscheme
-    { "rose-pine/neovim", name = "rose-pine" },
+  -- colorscheme
+  { "rose-pine/neovim", name = "rose-pine", opts = {transparent = true} },
+  { "LuRsT/austere.vim" },
+  { "kdheepak/monochrome.nvim" },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+  -- telescope
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- treesitter
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-    { 'nvim-treesitter/playground' },
+  -- treesitter
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'nvim-treesitter/playground' },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- harpoon
-    -- {
-    --     "ThePrimeagen/harpoon",
-    --     branch = "harpoon2",
-    --     dependencies = { "nvim-lua/plenary.nvim" }
-    -- },
+  -- harpoon
+  -- {
+  --     "ThePrimeagen/harpoon",
+  --     branch = "harpoon2",
+  --     dependencies = { "nvim-lua/plenary.nvim" }
+  -- },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- undo tree
-    {
-        'mbbill/undotree',
-    },
+  -- undo tree
+  {
+    'mbbill/undotree',
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- git integration
-    {
-      'tpope/vim-fugitive',
-    },
+  -- git integration
+  {
+    'tpope/vim-fugitive',
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- lsp
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            "folke/lazydev.nvim",
-            ft = "lua", -- only load on lua files
-            opts = {
-                library = {
-                    -- See the configuration section for more details
-                    -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
+  -- lsp
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
+      },
     },
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- mason
-    {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-    },
+  -- mason
+  {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- linter
-    {
-      'mfussenegger/nvim-lint',
-    },
+  -- linter
+  {
+    'mfussenegger/nvim-lint',
+  },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- completion
-    {
-        'hrsh7th/cmp-nvim-lsp',
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-            -- follow latest release.
-            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp"
-        }
-    },
-    { 'hrsh7th/nvim-cmp' },
-    { 'github/copilot.vim' },
+  -- completion
+  {
+    'hrsh7th/cmp-nvim-lsp',
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp"
+    }
+  },
+  { 'hrsh7th/nvim-cmp' },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- golf
-    { 'vuciv/golf' },
+  -- golf
+  { 'vuciv/golf' },
 
-    -------------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------
 
-    -- surround
-    { "tpope/vim-surround" },
+  -- surround
+  { "tpope/vim-surround" },
+
+  -------------------------------------------------------------------------------------
+
+  -- keycast
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      maxKeys = 4,
+      position = "top-right",
+    }
+  }
+
 })
