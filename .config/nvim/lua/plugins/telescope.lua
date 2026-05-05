df85cdccfd8552071da7b4be9a2ce9732ce87f30
gosling <prayush.twayana@gmail.com>
2026-05-05 13:43:50 +0545

    add telescope for open tabs

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,4 +8,26 @@ vim.keymap.set('n', '<leader>pc', builtin.colorscheme, { desc = 'Telescope color
 vim.keymap.set('n', '<C-g>', builtin.git_files, { desc = 'Telescope git files' })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Grep search with live preview' })
 
+vim.keymap.set('n', '<leader>pt', function()
+  local pickers = require('telescope.pickers')
+  local finders = require('telescope.finders')
+  local conf = require('telescope.config').values
 
+  local tabs = vim.api.nvim_list_tabpages()
+  local tab_names = {}
+
+  for _, tab in ipairs(tabs) do
+    local win = vim.api.nvim_tabpage_get_win(tab)
+    local buf = vim.api.nvim_win_get_buf(win)
+    local name = vim.api.nvim_buf_get_name(buf)
+    table.insert(tab_names, name ~= "" and name or "[No Name]")
+  end
+
+  pickers.new({}, {
+    prompt_title = "Tabs",
+    finder = finders.new_table {
+      results = tab_names,
+    },
+    sorter = conf.generic_sorter({}),
+  }):find()
+end, { desc = 'Telescope tabs' })
